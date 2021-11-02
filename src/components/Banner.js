3767a2092c4e6c08d53cdd61e0893fe4bea58f9e
Valentin Hobusch <valentinhobusch+lw@gmail.com>
2021-11-02 14:45:40 +0100

    svg updates

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -1,4 +1,5 @@
 import people from "./../people.svg"
+import textlogo from "./../textlogo.svg"
 import EmailForm from "./EmailForm"
 
 const Banner = () => {
@@ -6,10 +7,12 @@ const Banner = () => {
     <div className="banner">
       <div className="hero">
         <div>
-          <h1>Hi, we're lofft, modern brand stewards for the digital generation.</h1>
+          <h1>Hi, we're<img class="intext-logo" alt="lofft" src={textlogo}></img>, modern brand stewards for the digital generation.</h1>
           <EmailForm/>
         </div>
-        <img src={people} alt="Illustration of people" className="people" />
+        <div className="img-container">
+          <img src={people} alt="Illustration of people" className="people" />
+        </div>
       </div>
     </div>
   )
