0304d287be9e33ccaa309fa4e55ed3c82e678624
Valentin Hobusch <valentinhobusch+lw@gmail.com>
2021-11-03 13:36:21 +0100

    people image and css

--- src/components/People.js
+++ src/components/People.js
@@ -1,9 +1,29 @@
+import {peopleJson} from '../people'
+const randomPeople = peopleJson.people
+  .map((value) => ({ value, sort: Math.random() }))
+  .sort((a, b) => a.sort - b.sort)
+  .map(({ value }) => value)
+
+const Person = ({person}) => {
+  return (
+    <div className="person">
+      <img className="person-image" src={process.env.PUBLIC_URL + '/people/' + person.image} alt={person.name}/>
+      <p className="person-name">{person.name}</p>
+      <p className="person-role">{person.role}</p>
+      <p className="button button-mint person-about">{person.name + " who?"}</p>
+    </div>
+  )
+}
+
 const People = () => {
   return (
     <div className="people">
-      <h3>We are you.</h3>
-      <p>We've all been the new kid in town before.</p>
-      <p>Which also means we know exactly how to make your life easier.</p>
+      <h3 className="people-tagline">We are you.</h3>
+      <p className="people-description">We've all been the new kid in town before.</p>
+      <p className="people-description">Which also means we know exactly how to make your life easier.</p>
+      <div className="people-container">
+        {randomPeople.map((person) => <Person key={"person" + person.name} person={person}/>)}
+      </div>
     </div>
   )
 }
