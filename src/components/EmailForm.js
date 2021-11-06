59ddc4de8e5f24a82f96bddb62f14a3f7d973db7
Valentin Hobusch <valentinhobusch+lw@gmail.com>
2021-11-06 15:13:21 +0100

    mailchimp integration, wihtout api key

--- src/components/EmailForm.js
+++ src/components/EmailForm.js
@@ -1,7 +1,9 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import Button from "./Button";
+import MailchimpSubscribe from "react-mailchimp-subscribe"
 
-const EmailForm = () => {
+const EmailForm = ({ status, message, onValidated }) => {
+
   const [email, setEmail] = useState("");
   const handleChange = ({target}) => {
     setEmail(target.value)
@@ -9,16 +11,48 @@ const EmailForm = () => {
 
   const handleSubmit = (e) => {
     e.preventDefault()
-    window.alert(`Thanks for signing up ${email} !`)
+    onValidated({EMAIL: email});
   }
 
+  useEffect(() => {
+    if(status === "success") setEmail("");
+    }, [status])
+
   return (
-    <form onSubmit={handleSubmit}>
-      <input type="email" value={email} onChange={handleChange} placeholder="email@domain.com"/>
-      <button type="submit" style={{display:"none"}}/>
-      <Button color="lavender" style={{fontSize: "1.5rem"}} onClick={handleSubmit}>Sign up</Button>
-    </form>
+    <div className="emailform-container">
+      <p className="emailform-message">
+        {status === "success" && "Thanks for signing up!"}
+        {status === "sending" && "processing..."}
+        {status === "error" && "something went wong"}
+        {status === null && "Be the first to get updates from us"}
+      </p>
+      <form onSubmit={handleSubmit}>
+        <input type="email" value={email} onChange={handleChange} placeholder="email@domain.com"/>
+        <button type="submit" style={{display:"none"}}/>
+        <Button color="lavender" style={{fontSize: "1.5rem"}} onClick={handleSubmit}>Sign up</Button>
+      </form>
+    </div>
   )
 }
 
-export default EmailForm;
+const MailchimpForm = props => {
+    // const url = `https://genhybridsystems.us1.list-manage.com/subscribe/post?u=${process.env.REACT_APP_MAILCHIMP_U}&id=${process.env.REACT_APP_MAILCHIMP_ID}`;
+    const url ="mailchimp.com"
+    return (
+        <div className="mailchimp-container">
+            <MailchimpSubscribe
+                url={url}
+                render={({ subscribe, status, message }) => (
+                    <EmailForm
+                        status={status}
+                        message={message}
+                        onValidated={formData => subscribe(formData)}
+                    />
+                )}
+            />
+        </div>
+
+    )
+}
+
+export default MailchimpForm;
