fde269846a2248599cafae0ebf944db9f865c168
Valentin Hobusch <valentinhobusch+lw@gmail.com>
2021-11-15 14:11:46 +0100

    james

--- src/people.js
+++ src/people.js
@@ -19,22 +19,24 @@ export const peopleJson = {
       "link": "http://google.com"
     },
     {
-      "name": "James",
-      "role": "Assistant magician & co-founder",
-      "image": "freddie.png",
-      "link": "http://google.com"
+      name: "James",
+      role: "Assistant magician & co-founder",
+      image: "james.png",
+      description: "James’ background is divided between aviation and development. Before co-founding Lofft, James worked as a First Officer for EasyJet (Europe) GmbH based at Berlin’s Tegel Airport and then Berlin Brandenburg Airport until he was made redundant in February 2021 due to the COVID-19 pandemic. Before working in aviation, James worked as a PHP and Laravel developer, working alongside and advising startups in their development journey. In 2014 James was involved in Google Startups Dublin developing a product for the aviation industry, which went on to win that round of Google Startups in Dublin. James embodies tech, vision and responsibility like no one else. He is an avid spokesman for the LGBTQ community, working with various NGOs and Charities to support and improve society.",
+      animal: 3,
+      vacation: 2,
+      drink: 6,
+      media: 5,
+      food: 2,
+      links: [
+
+      ]
     },
     {
-      "name": "Luke",
-      "role": "Assistant magician & co-founder",
-      "image": "freddie.png",
-      "link": "http://google.com"
-    },
-    {
-      "name": "Valentin",
-      "role": "Assistant magician & co-founder",
-      "image": "freddie.png",
-      "link": "http://google.com"
+      name: "Valentin",
+      role: "Assistant magician & co-founder",
+      image: "freddie.png",
+      link: "http://google.com"
     }
     ,{
       "name": "Ivo",
