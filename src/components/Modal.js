d0f548273611e050e6d24367a50132d89812d20a
Valentin Hobusch <valentinhobusch+lw@gmail.com>
2021-11-17 17:48:23 +0100

    toggle modal if click outside popup

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -1,18 +1,26 @@
 import {popups} from '../popups'
 
+
 const Modal = ({data, toggleModal}) => {
+
+  const clickModal = ({target}) => {
+    if (target.className === "modal") {
+      toggleModal();
+    }
+  }
 
   if(data.type === "tag") {
     return (
-      <div className="modal">
+      <div className="modal" onClick={clickModal}>
        <div className="popup">
         <img
         src={process.env.PUBLIC_URL + '/icons/close-icon.png'}
+        className="popup-close"
         alt="X"
         onClick={toggleModal}
       />
          <h2>{popups[data.key].title}</h2>
-         <img src={process.env.PUBLIC_URL + '/illustrations/' + popups[data.key].image} alt="handshake"/>
+         <img src={process.env.PUBLIC_URL + '/illustrations/' + popups[data.key].image} alt="illustration"/>
          <p>{popups[data.key].text}</p>
        </div>
       </div>
