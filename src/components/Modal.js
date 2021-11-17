cdde58bca34d8388510490826a6c4a6efb5c08c4
Valentin Hobusch <valentinhobusch+lw@gmail.com>
2021-11-17 15:15:55 +0100

    popup styling with handshake default

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -1,7 +1,13 @@
+import handshake from "./../handshake.svg";
+
 const Modal = () => {
   return (
     <div className="modal">
-     <p>MODAL YO</p>
+     <div className="popup">
+       <h2>Meet your people</h2>
+       <img src={handshake} alt="handshake"/>
+       <p>Our unique algorithm matches you with the flatmates that suit you best. You choose what's important to you — and what information you wish to share about yourself. Lofft will do the rest, and present you with a shortlist of only the best places or candidates.</p>
+     </div>
     </div>
   )
 }
