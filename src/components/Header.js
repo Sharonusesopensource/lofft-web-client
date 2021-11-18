099a063104710ff4f4a4ef5dd4bb5c8426e2a5ea
Valentin Hobusch <valentinhobusch+lw@gmail.com>
2021-11-18 12:19:54 +0100

    remove nav

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,13 +1,10 @@
-
 import CountDownToLaunch from "./countdown";
 
-const Header = ({toggle, isLanding}) => {
+const Header = () => {
   return (
     <header>
       <img src={process.env.PUBLIC_URL + '/illustrations/logo.svg'} alt="Lofft Logo" className="logo" />
       <div className="nav">
-        <h3 onClick={()=>toggle()}className={`nav-link ${isLanding ? "nav-active" : ""}`}>About us</h3>
-        <h3 onClick={()=>toggle()}className={`nav-link ${!isLanding ? "nav-active" : ""}`}>For Investors</h3>
         <CountDownToLaunch/>
      </div>
     </header>
