490cdcff1974d14efa4aa7be75e0fb1b3a896127
Valentin Hobusch <valentinhobusch+lw@gmail.com>
2021-11-20 16:48:28 +0100

    basic footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,6 +1,16 @@
 const Footer = () => {
   return (
     <footer>
+      <div className="footer-sections">
+        <div className="footer-section">
+          <p>Contact us</p>
+          <a href="mailto:hello@lofft.app" target="_blank" rel="noreferrer">hello@lofft.app</a>
+        </div>
+        <div className="footer-section">
+          <p>Follow us</p>
+          <a  href="https://www.linkedin.com/company/lofftapp" target="_blank" rel="noreferrer">LinkedIn</a>
+        </div>
+      </div>
       <p style={{fontSize: "1rem", textAlign: "center"}}>Made with 🖤 in Berlin</p>
     </footer>
   )
