dbd94a82f11a53775b68f9252ed721136001c190
Valentin Hobusch <valentinhobusch+lw@gmail.com>
2021-11-20 16:33:02 +0100

    people popup responsive

--- src/people.js
+++ src/people.js
@@ -2,7 +2,7 @@ export const peopleJson = {
   people: [
     {
       name: "Freddie",
-      role: "Assistant magician & co-founder",
+      role: "Co-founder",
       image: "freddie.png",
       description: "Freddie (Nattapon Sukprasong) is a digital product designer, specialising in product discoveries, design strategy, and creating delightful user experience & interface. With educational background in anthropology and professional experience in social research, Freddie's well-equipped with research methods to understand complex human needs.",
       animal: 2,
@@ -30,7 +30,7 @@ export const peopleJson = {
     },
     {
       name: "Adam",
-      role: "Assistant magician & co-founder",
+      role: "Co-founder",
       image: "adam.png",
       description: "",
       animal: 6,
@@ -44,7 +44,7 @@ export const peopleJson = {
     },
     {
       name: "Leo",
-      role: "Assistant magician & co-founder",
+      role: "Developer",
       image: "leo.png",
       description: "",
       animal: 1,
@@ -58,7 +58,7 @@ export const peopleJson = {
     },
     {
       name: "James",
-      role: "Assistant magician & co-founder",
+      role: "Co-founder",
       image: "james.png",
       description: "James’ background is divided between aviation and development. Before co-founding Lofft, James worked as a First Officer for EasyJet (Europe) GmbH based at Berlin’s Tegel Airport and then Berlin Brandenburg Airport until he was made redundant in February 2021 due to the COVID-19 pandemic. Before working in aviation, James worked as a PHP and Laravel developer, working alongside and advising startups in their development journey. In 2014 James was involved in Google Startups Dublin developing a product for the aviation industry, which went on to win that round of Google Startups in Dublin. James embodies tech, vision and responsibility like no one else. He is an avid spokesman for the LGBTQ community, working with various NGOs and Charities to support and improve society.",
       animal: 3,
@@ -72,7 +72,7 @@ export const peopleJson = {
     },
     {
       name: "Valentin",
-      role: "Assistant magician & co-founder",
+      role: "Lead Developer",
       image: "valentin.png",
       description: "Valentin studied computer Science at TU Berlin. He's currently teaching creative people programming at LeWagon.",
       animal: 2,
@@ -85,12 +85,17 @@ export const peopleJson = {
           icon: "linkedin.svg",
           url: "https://www.linkedin.com/in/valentin-hobusch",
           alt: "Linked In"
+        },
+        {
+          icon: "github.svg",
+          url: "https://github.com/valentinLW/",
+          alt: "GitHub"
         }
       ]
     },
     {
       name: "Ivo",
-      role: "Assistant magician & co-founder",
+      role: "Developer",
       image: "ivo.png",
       description: "Ivo studied Business Informatics at TU Berlin but he's most passionate about silly hobby programming projects whether that's scraping data, automation or creation of bots. He loves supporting others in their journeys to learn coding and helping them find joy in programming which brought him to teaching at Le Wagon.",
       animal: 9,
