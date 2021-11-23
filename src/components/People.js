a99bdfd69a65ef06e523a84e1a8d8aa48517ffa1
James Hibbeard <jhibbeard@gmail.com>
2021-11-23 21:21:03 +0100

    Added the scroll disable to the people popups

--- src/components/People.js
+++ src/components/People.js
@@ -1,14 +1,13 @@
-import React from 'react';
-import Button from "./Button"
-import { peopleJson } from "../people"
-
+import React from "react";
+import Button from "./Button";
+import { peopleJson } from "../people";
+import { scrollDissable } from "../scrolltoggle";
 const randomPeople = peopleJson.people
   .map((value) => ({ value, sort: Math.random() }))
   .sort((a, b) => a.sort - b.sort)
-  .map(({ value }) => value)
+  .map(({ value }) => value);
 
 const Person = ({ person, setModal }) => {
-
   return (
     <div className="person">
       <img
@@ -19,29 +18,40 @@ const Person = ({ person, setModal }) => {
       <p className="person-name">{person.name}</p>
       <p className="person-role">{person.role}</p>
       <Button
-        onClick={() => setModal({type: "person", key: person})}
+        onClick={() => {
+          scrollDissable();
+          setModal({ type: "person", key: person });
+        }}
         color="mint"
         style={{ width: "fit-content", margin: "0 auto" }}
       >
         {person.name + " who?"}
       </Button>
     </div>
-  )
-}
+  );
+};
 
-const People = ({setModal}) => {
+const People = ({ setModal }) => {
   return (
     <div className="people-wrapper">
       <h3 className="section-tagline">We are you</h3>
-      <p className="section-description">We've all been the new kid in town before.</p>
-      <p className="section-description">Which also means we know exactly how to make your life easier.</p>
+      <p className="section-description">
+        We've all been the new kid in town before.
+      </p>
+      <p className="section-description">
+        Which also means we know exactly how to make your life easier.
+      </p>
       <div className="people-container">
         {randomPeople.map((person) => (
-          <Person key={"person" + person.name} person={person} setModal={setModal}/>
+          <Person
+            key={"person" + person.name}
+            person={person}
+            setModal={setModal}
+          />
         ))}
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default People
+export default People;
