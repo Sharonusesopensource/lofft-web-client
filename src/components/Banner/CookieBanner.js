c63ec2ad61d6e6e20ec1f8e56cf8ea2544238fb5
Valentin Hobusch <valentinhobusch+lw@gmail.com>
2021-11-26 14:57:03 +0100

    cookie banner buttons

--- src/components/Banner/CookieBanner.js
+++ src/components/Banner/CookieBanner.js
@@ -1,9 +1,12 @@
+import Button from "../Button/Button";
 import "./cookieBanner.scss";
 
-const CookieBanner = () => {
+const CookieBanner = ({cookies}) => {
   return (
     <div className="cookiebanner">
-      <p>COKKIE???</p>
+      <p>Want some cookies? We use cookies to ensure that we give you the best experience on our website, Please let us know if you agree to our cookies.</p>
+      <Button color="mint">I dont want cookies</Button>
+      <Button color="lavender">Accept & continue</Button>
     </div>
   );
 };
