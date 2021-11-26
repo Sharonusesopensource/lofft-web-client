a76ee6d5bee6f95f5c8017c4ed43265b282cce93
Valentin Hobusch <valentinhobusch+lw@gmail.com>
2021-11-26 15:34:32 +0100

    cookie banner logic

--- src/App.js
+++ src/App.js
@@ -16,18 +16,19 @@ const tagManagerArgs = {
 
 
 function App() {
-
-  useEffect(() => {
-    console.log("GTM CHECK")
-    if(cookies.get('cookie-consent') === "true") {
-      console.log("accepted")
-      TagManager.initialize(tagManagerArgs);
-    } else {
-      console.log("denied/unset")
-    }
-  }, [])
   const [showModal, setShowModal] = useState(false);
   const [modalData, setModalData] = useState({ type: null, key: null });
+  const [showCBanner, setShowCBanner] = useState(() => {
+    return !cookies.get('cookie-consent')
+  })
+
+  useEffect(() => {
+    if(cookies.get('cookie-consent') === "true") {
+      TagManager.initialize(tagManagerArgs);
+    }
+  }, [showCBanner])
+
+
   const toggleModal = () => {
     setShowModal((prevState) => !prevState);
   };
@@ -37,12 +38,17 @@ function App() {
     toggleModal();
   };
 
+  const handleCookieBanner = (arg) => {
+    setShowCBanner(false)
+    cookies.set('cookie-consent', arg)
+  }
+
   return (
     <div className="App">
       <Header />
       <LandingPage setModal={setModal} />
       {showModal && <Modal data={modalData} toggleModal={toggleModal} />}
-      <CookieBanner />
+      {showCBanner && <CookieBanner cookies={cookies} handleBanner={handleCookieBanner}/>}
     </div>
   );
 }
--- src/components/Banner/CookieBanner.js
+++ src/components/Banner/CookieBanner.js
@@ -1,12 +1,12 @@
 import Button from "../Button/Button";
 import "./cookieBanner.scss";
 
-const CookieBanner = ({cookies}) => {
+const CookieBanner = ({handleBanner}) => {
   return (
     <div className="cookiebanner">
       <p>Want some cookies? We use cookies to ensure that we give you the best experience on our website, Please let us know if you agree to our cookies.</p>
-      <Button color="mint">I dont want cookies</Button>
-      <Button color="lavender">Accept & continue</Button>
+      <Button onClick={() => handleBanner("false")} color="mint">I dont want cookies</Button>
+      <Button onClick={() => handleBanner("true")} color="lavender">Accept & continue</Button>
     </div>
   );
 };
