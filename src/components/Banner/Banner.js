7d03efef96b6e02a303dc539c0a51800f3e4c033
Valentin Hobusch <valentinhobusch+lw@gmail.com>
2021-11-30 14:36:28 +0100

    i18n setup

--- src/components/Banner/Banner.js
+++ src/components/Banner/Banner.js
@@ -2,15 +2,18 @@ import React from "react";
 import MailchimpForm from "../Forms/EmailForm";
 import "./banner.scss";
 import textLogo from "../../assets/illustrations/textlogo.svg";
+import { useTranslation } from 'react-i18next';
 
 const Banner = () => {
+  const { t } = useTranslation();
+
   return (
     <div className="banner">
       <div className="hero">
         <div>
           <h1>
-            <img className="intext-logo" alt="lofft" src={textLogo}></img>, the
-            brand new way to manage shared flats and accommodation - together.
+            <img className="intext-logo" alt="lofft" src={textLogo}></img>
+            {t('banner.message')}
           </h1>
           <MailchimpForm />
         </div>
