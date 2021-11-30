bb6d5acf7333c0405efee9ca44f7d2952b9cb8a4
Valentin Hobusch <valentinhobusch+lw@gmail.com>
2021-11-30 16:39:49 +0100

    add browser language detection

--- src/i18n.js
+++ src/i18n.js
@@ -1,10 +1,14 @@
 import i18n from 'i18next';
 import { initReactI18next } from 'react-i18next';
+import LanguageDetector from 'i18next-browser-languagedetector';
 
 import translationEN from './locales/en.json';
 import translationDE from './locales/de.json';
 
 i18n
+  // detect user language
+  // learn more: https://github.com/i18next/i18next-browser-languageDetector
+  .use(LanguageDetector)
   // pass the i18n instance to react-i18next.
   .use(initReactI18next)
   // init i18next
