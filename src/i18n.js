87b712d698d9612b055339c32e0789dd9e34a5a4
Valentin Hobusch <valentinhobusch+lw@gmail.com>
2021-12-01 11:21:46 +0100

    popup text hotfix

--- src/i18n.js
+++ src/i18n.js
@@ -1,20 +1,20 @@
 import i18n from 'i18next';
 import { initReactI18next } from 'react-i18next';
-import LanguageDetector from 'i18next-browser-languagedetector';
+// import LanguageDetector from 'i18next-browser-languagedetector';
 
 import translationEN from './locales/en.json';
-import translationDE from './locales/de.json';
+// import translationDE from './locales/de.json';
 
 i18n
   // detect user language
   // learn more: https://github.com/i18next/i18next-browser-languageDetector
-  .use(LanguageDetector)
+  // .use(LanguageDetector)
   // pass the i18n instance to react-i18next.
   .use(initReactI18next)
   // init i18next
   // for all options read: https://www.i18next.com/overview/configuration-options
   .init({
-    debug: true,
+    debug: false,
     fallbackLng: 'en',
     interpolation: {
       escapeValue: false, // not needed for react as it escapes by default
@@ -22,10 +22,11 @@ i18n
     resources: {
       en: {
         translation: translationEN
-      },
-      de: {
-        translation: translationDE
       }
+      // ,
+      // de: {
+      //   translation: translationDE
+      // }
     }
   });
 
