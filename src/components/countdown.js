8c8eb9927bb368106291f6bffa0b7665e549f2f8
James Hibbeard <jhibbeard@gmail.com>
2021-12-28 11:15:44 +0000

    Updated yarn and date to 01 March 2022 as new launch

--- src/components/countdown.js
+++ src/components/countdown.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from "react";
-import { useTranslation } from 'react-i18next';
+import { useTranslation } from "react-i18next";
 
-const countDownDate = new Date("Jan 1, 2022 23:59:59").getTime();
+const countDownDate = new Date("Mar 1, 2022 23:59:59").getTime();
 let Calculator = (m = 1, s = 1, h = 1) => {
   return 1000 * m * s * h;
 };
@@ -36,10 +36,11 @@ let CountDownToLaunch = () => {
 
   return (
     <h3 id="countdown">
-      {`${dateTarget[0]}${t('countdown.days')} : ${dateTarget[1]}${t('countdown.hours')} : ${dateTarget[2]}${t('countdown.minutes')}`}
+      {`${dateTarget[0]}${t("countdown.days")} : ${dateTarget[1]}${t(
+        "countdown.hours"
+      )} : ${dateTarget[2]}${t("countdown.minutes")}`}
       <span>
-        {t('countdown.until')}
-        {" "}
+        {t("countdown.until")}{" "}
         <span role="img" aria-label="rocket">
           🚀
         </span>
